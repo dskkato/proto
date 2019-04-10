a935e9b60a1f45adc5cbc2bb7f4632b855b99cfe
Daisuke Kato <kato.daisuke429@gmail.com>
2019-04-11 05:03:20 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="proto",
+        version="0.1",
+        description="Prototype project",
+        author="Daisuke Kato",
+        author_email="kato.daisuke429@gmail.com",
+        license="MIT",
+        packages=["proto"],
+        zip_safe=False)
